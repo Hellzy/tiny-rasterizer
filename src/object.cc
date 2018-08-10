72edd0c08e0a4e7aed731afe22cda95064795e8b
Francois Mazeau <mazeau_f@epita.fr>
2018-08-10 18:52:53 +0200

    linear: object: add virtual pure projections

--- src/object.cc
+++ src/object.cc
@@ -1,11 +1,19 @@
 #include <random>
+
 #include "object.hh"
 
 Object::Object()
 {
-  std::random_device rd;
-  std::mt19937 gen(rd());
-  std::uniform_real_distribution<> dis(0.0, 1.0);
+    static int count = 0;
+    static color_t cur_col;
 
-  col = {dis(gen), dis(gen), dis(gen)};
+    if (!(count++ % 2))
+    {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<> dis(0.0, 1.0);
+        cur_col = {dis(gen), dis(gen), dis(gen)};
+    }
+
+    col = cur_col;
 }
--- src/object.hh
+++ src/object.hh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include <vector>
 
 #include "types.hh"
 
@@ -11,7 +12,16 @@ struct Object
 {
   Object();
 
+  virtual void cam_project(const cam_t& cam) = 0;
+  virtual void screen_project(size_t width, size_t height) = 0;
+  virtual void raster_project(size_t width, size_t height) = 0;
+  virtual void ndc_project(double l, double r, double b, double t) = 0;
+  virtual bool check_edges(const point_t& p) = 0;
+
+  virtual std::vector<point_t> get_points() const = 0;
 
   /** Color for the light, absorbtion for the primitives **/
   color_t col;
 };
+
+using object_ptr_t = std::shared_ptr<Object>;
