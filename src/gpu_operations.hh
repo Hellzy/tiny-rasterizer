fecd1c3a90dc6223f3c98f2d874ab8a1f951676c
Francois Mazeau <mazeau_f@epita.fr>
2018-08-15 22:04:10 +0200

    gpu: add gpu_operations header
    This header defines the cuda specific functions that will be
    used for the gpu version of the rasterizer

--- src/gpu_operations.hh
+++ src/gpu_operations.hh
@@ -3,4 +3,26 @@
 
 #include "utils.hh"
 
+/*
+ * Projects every point of the scene in raster space
+ *
+ * - points: array of all the points in the scene
+ * - point_nb: length of the array of points
+ * - cam: the camera looking at the scene
+ * - screen_w: width of the screen
+ * - screen_h: height of the screen
+ */
 void projection_kernel(point_t* points, size_t point_nb, const cam_t& cam, size_t screen_w, size_t screen_h);
+
+/*
+ * Draws the scene on the screen
+ *
+ * - screen: framebuffer to draw on
+ * - screen_h: screen height
+ * - screen_w: screen width
+ * - meshes: meshes of the scene
+ * - z_buffer: depth buffer
+ */
+void draw_mesh_kernel(std::vector<color_t>& screen, size_t screen_h, size_t screen_w,
+        const std::vector<mesh_t>& meshes, const std::vector<double>& z_buffer);
+
