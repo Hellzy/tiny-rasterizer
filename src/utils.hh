5ab8b340357b877a1deae2ba6a535369c0014634
Francois Mazeau <mazeau_f@epita.fr>
2018-08-26 01:51:05 +0200

    gpu: remove preprocessing conditions in types.hh

--- src/utils.hh
+++ src/utils.hh
@@ -4,40 +4,19 @@
 
 #include "types.hh"
 
-template <typename T, unsigned dim2>
-void mat_mult(T m1[9], T m2[dim2], T out[dim2])
-{
-    int out_off = 0;
-
-    for (unsigned i = 0; i < 3; ++i)
-    {
-        for (unsigned j = 0; j < dim2 / 3; j++)
-        {
-            for (unsigned k = 0; k < 3; ++k)
-                out[out_off] += m1[i * 3 + k] * m2[k * 3 / dim2 + j];
-
-            ++out_off;
-        }
-    }
-}
-
 /**
  * e1:  first edge point
  * e2:  second edge point
  */
 inline
-#ifdef GPU
 __device__
-#endif
 double edge_function(point_t e1, point_t e2, point_t p)
 {
     return ((p.x - e1.x) * (e2.y - e1.y) - (p.y - e1.y) * (e2.x - e1.x));
 }
 
 inline
-#ifdef GPU
 __device__
-#endif
 bool check_edges(const point_t& p1, const point_t& p2, const point_t& p3,
         const point_t& p)
 {
